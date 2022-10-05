da1ec01ae364cb485853b5e5a5069d4d4cc39585
Ananthapadmanaban S <anandsivesh@gmail.com>
2022-10-05 21:56:57 +0530

    commiting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,150 @@
 import logo from './logo.svg';
 import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import Navbar from './navbar';
+import Pricing from './PricingInfo';
+import Card from './card';
 
 function App() {
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container py3">
+      <header>
+        <Navbar></Navbar>
+        <Pricing></Pricing>
       </header>
+      <main>     
+      <Card></Card>
+      </main>
+
     </div>
   );
 }
 
 export default App;
+
+ 
+// // let details = [
+//   //   {
+//   //     subs: "FREE",
+//   //     price: 0,
+//   //     benefits: [
+//   //       {
+//   //         type: "Single User"
+//   //       },
+//   //       {
+//   //         type: "5GB Storage"
+//   //       },
+//   //       {
+//   //         type: "Unlimited Public Projects"
+//   //       },
+//   //       {
+//   //         type: "Community Access"
+//   //       },
+//   //       {
+//   //         type: "Unlimited Private Projects"
+//   //       },
+//   //       {
+//   //         type: "Dedicated Phone Support"
+//   //       },
+
+//   //       {
+//   //         type: "Free Subdomain"
+//   //       },
+//   //       {
+//   //         type: "Monthly Status Reports"
+//   //       }
+//   //     ]
+//   //   },
+
+//   //   {
+//   //     subs: "PLUS",
+//   //     price: 9,
+//   //     benefits: [
+//   //       {
+//   //         type: "5 Users"
+//   //       },
+//   //       {
+//   //         type: "50GB Storage"
+//   //       },
+//   //       {
+//   //         type: "Unlimited Public Projects"
+//   //       },
+//   //       {
+//   //         type: "Community Access"
+//   //       },
+//   //       {
+//   //         type: "Unlimited Private Projects"
+//   //       },
+//   //       {
+//   //         type: "Dedicated Phone Support"
+//   //       },
+
+//   //       {
+//   //         type: "Free Subdomain"
+//   //       },
+//   //       {
+//   //         type: "Monthly Status Reports"
+//   //       }
+
+
+//   //     ]
+
+
+//   //   },
+
+//   //   {
+//   //     subs: "PRO",
+//   //     price: 49,
+//   //     benefits: [
+//   //       {
+//   //         type: "Unlimited Users"
+//   //       },
+//   //       {
+//   //         type: "150GB Storage"
+//   //       },
+//   //       {
+//   //         type: "Unlimited Public Projects"
+//   //       },
+//   //       {
+//   //         type: "Community Access"
+//   //       },
+//   //       {
+//   //         type: "Unlimited Private Projects"
+//   //       },
+//   //       {
+//   //         type: "Dedicated Phone Support"
+//   //       },
+
+//   //       {
+//   //         type: "Free Subdomain"
+//   //       },
+//   //       {
+//   //         type: "Monthly Status Reports"
+//   //       }
+
+
+//   //     ]
+
+
+//   //   }
+//   // ]
+
+
+
+// {/* <section className="pricing py-5">
+//           <div className="container">
+            
+
+//               {details.map((items) => {
+//                 return (
+//                   <Card
+//                     items={items}
+//                   ></Card>
+//                 )
+//               })
+//               }
+            
+//           </div>
+//         </section> */}
---
+++ src/PricingInfo.js
@@ -0,0 +1,11 @@
+function Pricing() {
+    return (
+        <div className="pricing-header p-3 pb-md-4 mx-auto text-center">
+            <h1 className="display-4 fw-normal">Enjoy the Benefits</h1>
+            <p className="fs-5 text-muted">Try our packages exclusively for you and your family . There are some new features available on this updated version.</p>
+        </div>
+
+    )
+}
+
+export default Pricing;
